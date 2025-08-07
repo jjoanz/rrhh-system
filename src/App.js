3ee1f7593884974a03cf2e57d892a693db03ba6a
Jose Montedeoca <jjoanz0618@gmail.com>
2025-08-07 16:48:25 -0400

    Update title - test from PC2

--- src/App.js
+++ src/App.js
@@ -193,7 +193,7 @@ const Header = () => {
             alt="Logo"
             style={{ height: '24px', width: 'auto' }}
           />
-          Sistema de Gestión RRHH ProDominicana v2.0 - PC2
+          Sistema de Gestión RRHH ProDominicana v2.0 - PC3
         </h1>
 
         </div>

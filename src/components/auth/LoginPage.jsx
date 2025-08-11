0a20b509248d6d042a4fb981c2df16428ead6e18
Jose Montedeoca <jjoanz0618@gmail.com>
2025-08-11 11:57:41 -0400

    Actualización Login quitando los usuarios

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useApp } from '../../context/AppContext';
-import { DEMO_USERS } from '../../data/users';
+import { DEMO_USERS } from '../../';
 import { 
   Building2, 
   User, 
